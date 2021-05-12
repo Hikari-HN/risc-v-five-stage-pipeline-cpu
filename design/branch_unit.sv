2c46e5866f233040c7124b293c371d4f645a1a3e
黎梓浩 <1245949348@qq.com>
2021-05-12 14:51:45 +0800

    5/12/14;51

--- design/branch_unit.sv
+++ design/branch_unit.sv
@@ -13,6 +13,17 @@ module BranchUnit #(
     output logic [31:0]        branch_target,   // BrPC
     output logic               pc_sel
 );
-
+    
+    logic [31:0] pc = {23'b0, cur_pc};
+    always_comb
+    begin
+        pc_plus_4 = pc + 32'd4;
+        pc_plus_imm = pc + imm;
+        pc_sel = jalr_sel | (branch_taken & alu_result[0]);
+        if (jalr_sel == 1'b1)
+            branch_target = alu_result & 32'hfffffffe; // jalr
+        else
+            branch_target = pc + (imm << 1); // jal and beq
+    end
 
 endmodule

39365e3b2da2001bc9992970d8f54dfc73c5dc48
黎梓浩 <1245949348@qq.com>
2021-05-13 21:17:21 +0800

    alu_controller

--- design/alu_controller.sv
+++ design/alu_controller.sv
@@ -10,39 +10,32 @@ module ALU_Controller (
     // add your code here.
     always_comb
 	    case (alu_op)
-		    2'b00:                   operation = 4'b0000; // lw, sw
+		    2'b00:                      operation = 4'b0000; // lw, sw
             2'b01:
                 case (funct3)
-                    3'b000:          operation = 4'b0101; // beq
-                    3'b001:	         operation = 4'b0110; // bne
-                    3'b100:          operation = 4'b0111; // blt
-                    3'b101:          operation = 4'b1000; // bge
-                    3'b110:          operation = 4'b1001; // bltu
-                    3'b111:          operation = 4'b1010; // bgeu
-                    default:         operation = 4'b0000;
+                    3'b000:             operation = 4'b0101; // beq
+                    3'b001:	            operation = 4'b0110; // bne
+                    3'b100:             operation = 4'b0111; // blt
+                    3'b101:             operation = 4'b1000; // bge
+                    3'b110:             operation = 4'b1001; // bltu
+                    3'b111:             operation = 4'b1010; // bgeu
+                    default:            operation = 4'b0000;
                 endcase
 		    2'b10:
-		        case (funct7)
-                    7'b0000000:
-                        case (funct3)
-                            3'b000:  operation = 4'b0000; // add
-                            3'b100:  operation = 4'b1100; // xor
-                            3'b110:  operation = 4'b0010; // or
-                            3'b111:  operation = 4'b0011; // and
-                            3'b010:  operation = 4'b0100; // slt
-                            default: operation = 4'b0000;
+		        case (funct3)
+                    3'b000:
+                        case (funct7)
+                            7'b0100000: operation = 4'b0001; // sub (存在addi的立即数恰好满足此情况的bug)
+                            default:    operation = 4'b0000; // add, addi
                         endcase
-                    7'b0100000:      operation = 4'b0001; // sub
-                    default:
-                        case (funct3)
-                            3'b000:  operation = 4'b0000; // addi
-                            3'b110:  operation = 4'b0010; // ori
-                            3'b111:  operation = 4'b0011; // andi
-                            default: operation = 4'b0000;   
-                        endcase                        
-                endcase   
-            2'b11:                   operation = 4'b1011; // jal
-            default:                 operation = 4'b0000;
+                    3'b100:             operation = 4'b1100; // xor
+                    3'b110:             operation = 4'b0010; // or, ori
+                    3'b111:             operation = 4'b0011; // and, andi
+                    3'b010:             operation = 4'b0100; // slt
+                    default:            operation = 4'b0000;
+                endcase 
+            2'b11:                      operation = 4'b1011; // jal
+            default:                    operation = 4'b0000;
 	    endcase
 
 endmodule
